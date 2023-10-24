acd3d46e33c7d211de2c08d20f39cb6e9b6e9a71
jain-hemant <hemantjain1321@gmail.com>
2023-10-24 21:41:05 +0530

    HOF Assignment

--- Javascript/HOF/even_index.js
+++ Javascript/HOF/even_index.js
@@ -1,7 +1,8 @@
 let arr = [3,4,5,6,7]
-let ans = arr.filter((ele,index)=>{
+let ans = []
+arr.filter((ele,index)=>{
     if(index%2==0){
-        console.log(ele)
+        ans.push(ele)
     }
 })
-// console.log(ans)
+console.log(ans.join("-"))
